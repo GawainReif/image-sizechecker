d6c7a753b4da24a9ec6b2f57585ee1e04815bf1a
Gawain Reifsnyder <gawain.reifsnyder@daveramsey.com>
2018-04-10 09:33:56 -0500

    Improve structure

--- id.rb
+++ id.rb
@@ -1,47 +1,59 @@
 #!/Users/gawain.reifsnyder/.rvm/rubies/ruby-2.2.2/bin/ruby
 
-require 'csv'
-
-
-class HashAssembler
+class ImageTester
   def initialize
     files = `ls `.split(" ")
-    csv_string = ""
-    calculator = Calculator.new
+    images = assemble(files)
+    add_ratio(images)
+
+    Calculator.calc_ratio(images)
+    puts images
+  end
+
+private
+
+  def add_ratio(images)
+    images.each do |img|
+      img[:ratio] = (img[:size].to_f / (img[:width].to_f * img[:height].to_f)).round(3)
+    end
+  end
+
+  def assemble(files)
+    image_data = []
     files.collect do |f|
       if File.extname(f) == ".jpg" || File.extname(f) == ".png"
         cmd = "magick identify -format \"%w %H %m %b %f\" \"#{f}\""
         params = `#{cmd}`.split(" ")
-
-        image_data = []
         if !params.include?(nil)
           hsh = [[:width, params[0]], [:height, params[1]], [:type, params[2]], [:size, params[3]], [:file, params[4]]].to_h
-
-          # ratio = hsh[:size].to_f / (hsh[:width].to_f * hsh[:height].to_f)
-          # hsh[:ratio] = ratio.round(3)
-
-          pixels = hsh[:width].to_f * hsh[:height].to_f
-          hsh[:pixels] = pixels.to_i
-
-          ratio = calculator.ratio(pixels, hsh[:size])
-          hsh[:ratio] = ratio
-
-          image_data.push(hsh)
         end
+        image_data.push(hsh)
       end
-      # Write to csv
-      csv_string += [pixels.to_i,ratio.round(3)].to_csv
-      puts image_data
     end
+    image_data
   end
 end
 
 class Calculator
-  def ratio(pixels, size)
-    (size.to_f / pixels).round(3)
+  ACCEPTABLE_MULTIPLIER = 1.2
+  TOO_BIG_MULTIPLIER = 2
+
+  def self.calc_ratio(images)
+    images.each do |img|
+      actual_ratio = img[:ratio]
+      ideal_ratio = (8.3053*(img[:size].to_i ** -0.33)).round(3)
+      acceptable_ratio = ideal_ratio * ACCEPTABLE_MULTIPLIER
+      too_big = ideal_ratio * TOO_BIG_MULTIPLIER
+
+      if(actual_ratio <= acceptable_ratio)
+        img[:status] = "green"
+      elsif(actual_ratio > acceptable_ratio && actual_ratio < too_big)
+        img[:status] = "yellow"
+      elsif(actual_ratio >= too_big)
+        img[:status] = "red"
+      end
+    end
   end
 end
 
-hash_assembler = HashAssembler.new
-
-# puts csv_string
+hash_assembler = ImageTester.new
